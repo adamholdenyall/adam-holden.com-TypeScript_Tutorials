49da57eb9fa069436c92595ea0dcf37203b17892
Adam Holden <adammholden@gmail.com>
2014-05-08 23:55:57 -0500

    Commented Items.ts

--- part2/ts/Items.js
+++ part2/ts/Items.js
@@ -7,6 +7,8 @@ var __extends = this.__extends || function (d, b) {
     d.prototype = new __();
 };
 define(["require", "exports", "lib/TypeState"], function(require, exports, TypeState) {
+    //The star can spin either clockwise or counter-clockwise.
+    //The direction will be toggled with a click event.
     (function (StarStates) {
         StarStates[StarStates["SpinningCW"] = 0] = "SpinningCW";
         StarStates[StarStates["SpinningCCW"] = 1] = "SpinningCCW";
@@ -22,13 +24,23 @@ define(["require", "exports", "lib/TypeState"], function(require, exports, TypeS
             this.anchor.set(.5, .5);
             this.inputEnabled = true;
 
+            //Set up all valid transitions. If you attempt to perform an
+            //invalid transition, TypeState will show an error.
             this.fsm.from(0 /* SpinningCW */).to(1 /* SpinningCCW */);
             this.fsm.from(1 /* SpinningCCW */).to(0 /* SpinningCW */);
 
+            //".on" is used to define code that will be performed when the FSM is
+            //switched to a particular state. You can use the "from" variable
+            //to perform a switch to do different actions based on the state that
+            //you are transitioning from.
             this.fsm.on(0 /* SpinningCW */, function (from) {
+                //Only add the event handler once so that it doesn't trigger when we
+                //leave the state.
                 _this.events.onInputDown.addOnce(function () {
-                    _this.fsm.go(1 /* SpinningCCW */);
+                    _this.fsm.go(1 /* SpinningCCW */); //Change states
                 }, _this);
+
+                //Refine the update function to rotate clockwise
                 _this.update = function () {
                     _this.angle += 180 * _this.game.time.elapsed / 1000;
                 };
@@ -38,11 +50,17 @@ define(["require", "exports", "lib/TypeState"], function(require, exports, TypeS
                 _this.events.onInputDown.addOnce(function () {
                     _this.fsm.go(0 /* SpinningCW */);
                 }, _this);
+
+                //Refine the update function to rotate counter-clockwise
                 _this.update = function () {
                     _this.angle -= 180 * _this.game.time.elapsed / 1000;
                 };
             });
             game.add.existing(this);
+
+            //Though the constructor takes in a state at which to start the FSM, the ".on"
+            //handler won't trigger upon initialization. All states are allowed to
+            //transition to themselves.
             this.fsm.go(0 /* SpinningCW */);
         }
         Star.preload = function (game, path) {
@@ -53,6 +71,9 @@ define(["require", "exports", "lib/TypeState"], function(require, exports, TypeS
     })(Phaser.Sprite);
     exports.Star = Star;
 
+    //The treasure chest has a more complicated FSM with 5 states. Locked, Closed, and
+    //Open will allow for interaction, and it will be in Opening and Closing as it
+    //animates.
     (function (ChestStates) {
         ChestStates[ChestStates["Locked"] = 0] = "Locked";
         ChestStates[ChestStates["Closed"] = 1] = "Closed";
@@ -73,6 +94,7 @@ define(["require", "exports", "lib/TypeState"], function(require, exports, TypeS
             this.animations.add("close", Phaser.Animation.generateFrameNames('chestcropped', 6, 1, '.png', 4), 20, false);
             this.fixPivot();
 
+            //List valid transitions
             this.fsm.from(0 /* Locked */).to(1 /* Closed */);
             this.fsm.from(1 /* Closed */).to(0 /* Locked */);
             this.fsm.from(1 /* Closed */).to(2 /* Opening */);
@@ -80,23 +102,33 @@ define(["require", "exports", "lib/TypeState"], function(require, exports, TypeS
             this.fsm.from(3 /* Open */).to(4 /* Closing */);
             this.fsm.from(4 /* Closing */).to(1 /* Closed */);
 
+            //Unlock state
+            //Will listen for the 'U' button and transition when it is pressed
             var unlockKey = game.input.keyboard.addKey(Phaser.Keyboard.U);
             this.fsm.on(0 /* Locked */, function (from) {
-                _this.play("open").stop();
+                _this.play("open").stop(); //Start at a closed animation frame.
                 _this.tint = 0x999999;
                 unlockKey.onDown.addOnce(function () {
                     _this.fsm.go(1 /* Closed */);
                 }, _this);
             }).onExit(0 /* Locked */, function (to) {
+                //onExit and onEnter are powerful methods on the FSM.
+                //Firstly, you can use them to prepare or clean up code
+                //when the state changes. Secondly, they must return a
+                //boolean value. If it is true, the transition will occur.
+                //If it is false, the FSM will stay in the current state!
+                //Remember to unregister key listeners when you change states!
                 unlockKey.onDown.removeAll();
                 _this.tint = 0xFFFFFF;
                 return true;
             });
 
+            //Closed State
+            //Similar to the previous state, but you can either open or re-lock
             var openKey = game.input.keyboard.addKey(Phaser.Keyboard.O);
             var lockKey = game.input.keyboard.addKey(Phaser.Keyboard.L);
             this.fsm.on(1 /* Closed */, function (from) {
-                _this.play("open").stop();
+                _this.play("open").stop(); //Start at a closed animation frame.
                 openKey.onDown.addOnce(function () {
                     _this.fsm.go(2 /* Opening */);
                 }, _this);
@@ -109,12 +141,17 @@ define(["require", "exports", "lib/TypeState"], function(require, exports, TypeS
                 return true;
             });
 
+            //Opening State
+            //This simply blocks interaction while the opening animation plays.
+            //When the animation finishes, it will transition to the Open state
+            //and allow interactivity again.
             this.fsm.on(2 /* Opening */, function (from) {
                 _this.play("open").onComplete.addOnce(function () {
                     _this.fsm.go(3 /* Open */);
                 }, _this);
             });
 
+            //Open state
             var closeKey = game.input.keyboard.addKey(Phaser.Keyboard.C);
             this.fsm.on(3 /* Open */, function (from) {
                 _this.play("close").stop();
@@ -126,6 +163,8 @@ define(["require", "exports", "lib/TypeState"], function(require, exports, TypeS
                 return true;
             });
 
+            //Closing State
+            //Same as the Opening state, but with the opposite animation
             this.fsm.on(4 /* Closing */, function (from) {
                 _this.play("close").onComplete.addOnce(function () {
                     _this.fsm.go(1 /* Closed */);
@@ -134,12 +173,16 @@ define(["require", "exports", "lib/TypeState"], function(require, exports, TypeS
 
             game.add.existing(this);
 
+            //Though the constructor takes in a state at which to start the FSM, the ".on"
+            //handler won't trigger upon initialization. All states are allowed to
+            //transition to themselves.
             this.fsm.go(0 /* Locked */);
         }
         Chest.preload = function (game, imagePath, xmlPath) {
             game.load.atlasXML(Chest.key, imagePath, xmlPath);
         };
 
+        //Code that is necessary when using trimmed, pivoted sprites.
         Chest.prototype.fixPivot = function () {
             if (this.animations.currentAnim) {
                 this.pivot.x = this.animations.currentAnim.currentFrame.spriteSourceSizeW / 2;
--- part2/ts/Items.ts
+++ part2/ts/Items.ts
@@ -1,8 +1,10 @@
 /// <reference path="../../def/phaser.d.ts"/>
 /// <reference path="../../def/require.d.ts" />
 
-import TypeState = require("lib/TypeState");
+import TypeState = require("lib/TypeState");//Include TypeState with RequireJS
 
+//The star can spin either clockwise or counter-clockwise.
+//The direction will be toggled with a click event.
 export enum StarStates {
 	SpinningCW,
 	SpinningCCW
@@ -23,13 +25,22 @@ export class Star extends Phaser.Sprite {
 		this.anchor.set(.5,.5);
 		this.inputEnabled = true;
 
+		//Set up all valid transitions. If you attempt to perform an
+		//invalid transition, TypeState will show an error.
 		this.fsm.from(StarStates.SpinningCW).to(StarStates.SpinningCCW);
 		this.fsm.from(StarStates.SpinningCCW).to(StarStates.SpinningCW);
 
+		//".on" is used to define code that will be performed when the FSM is
+		//switched to a particular state. You can use the "from" variable
+		//to perform a switch to do different actions based on the state that
+		//you are transitioning from.
 		this.fsm.on(StarStates.SpinningCW, (from: StarStates)=>{
+			//Only add the event handler once so that it doesn't trigger when we
+			//leave the state.
 			this.events.onInputDown.addOnce(()=>{
-				this.fsm.go(StarStates.SpinningCCW);
+				this.fsm.go(StarStates.SpinningCCW);//Change states
 			},this);
+			//Refine the update function to rotate clockwise
 			this.update = () => {this.angle += 180 * this.game.time.elapsed / 1000;}
 		});
 
@@ -37,13 +48,22 @@ export class Star extends Phaser.Sprite {
 			this.events.onInputDown.addOnce(()=>{
 				this.fsm.go(StarStates.SpinningCW);
 			},this);
+			//Refine the update function to rotate counter-clockwise
 			this.update = () => { this.angle -= 180 * this.game.time.elapsed / 1000;}
 		});
 		game.add.existing(this);
+
+		//Though the constructor takes in a state at which to start the FSM, the ".on"
+		//handler won't trigger upon initialization. All states are allowed to
+		//transition to themselves.
 		this.fsm.go(StarStates.SpinningCW);
+
 	}
 }
 
+//The treasure chest has a more complicated FSM with 5 states. Locked, Closed, and
+//Open will allow for interaction, and it will be in Opening and Closing as it
+//animates.
 export enum ChestStates {
 	Locked,
 	Closed,
@@ -69,6 +89,7 @@ export class Chest extends Phaser.Sprite {
 		this.animations.add("close", Phaser.Animation.generateFrameNames('chestcropped', 6, 1, '.png',4), 20, false);
 		this.fixPivot();
 
+		//List valid transitions
 		this.fsm.from(ChestStates.Locked).to(ChestStates.Closed);
 		this.fsm.from(ChestStates.Closed).to(ChestStates.Locked);
 		this.fsm.from(ChestStates.Closed).to(ChestStates.Opening);
@@ -76,23 +97,33 @@ export class Chest extends Phaser.Sprite {
 		this.fsm.from(ChestStates.Open).to(ChestStates.Closing);
 		this.fsm.from(ChestStates.Closing).to(ChestStates.Closed);
 
+		//Unlock state
+		//Will listen for the 'U' button and transition when it is pressed
 		var unlockKey = game.input.keyboard.addKey(Phaser.Keyboard.U);
 		this.fsm.on(ChestStates.Locked,(from: ChestStates) =>{
-			this.play("open").stop();
+			this.play("open").stop();//Start at a closed animation frame.
 			this.tint = 0x999999;
 			unlockKey.onDown.addOnce(()=>{
 				this.fsm.go(ChestStates.Closed);
 			},this);
 		}).onExit(ChestStates.Locked,(to:ChestStates) =>{
+			//onExit and onEnter are powerful methods on the FSM.
+			//Firstly, you can use them to prepare or clean up code
+			//when the state changes. Secondly, they must return a
+			//boolean value. If it is true, the transition will occur.
+			//If it is false, the FSM will stay in the current state!
+			//Remember to unregister key listeners when you change states!
 			unlockKey.onDown.removeAll();
 			this.tint = 0xFFFFFF;
 			return true;
 		});
 
+		//Closed State
+		//Similar to the previous state, but you can either open or re-lock
 		var openKey = game.input.keyboard.addKey(Phaser.Keyboard.O);
 		var lockKey = game.input.keyboard.addKey(Phaser.Keyboard.L);
 		this.fsm.on(ChestStates.Closed, (from: ChestStates)=>{
-			this.play("open").stop();
+			this.play("open").stop();//Start at a closed animation frame.
 			openKey.onDown.addOnce(()=>{
 				this.fsm.go(ChestStates.Opening);
 			},this);
@@ -105,12 +136,17 @@ export class Chest extends Phaser.Sprite {
 			return true;
 		});
 
+		//Opening State
+		//This simply blocks interaction while the opening animation plays.
+		//When the animation finishes, it will transition to the Open state
+		//and allow interactivity again.
 		this.fsm.on(ChestStates.Opening, (from: ChestStates)=>{
 			this.play("open").onComplete.addOnce(()=>{
 				this.fsm.go(ChestStates.Open);
 			},this);
 		});
 
+		//Open state
 		var closeKey = game.input.keyboard.addKey(Phaser.Keyboard.C);
 		this.fsm.on(ChestStates.Open,(from:ChestStates) =>{
 			this.play("close").stop();
@@ -122,6 +158,8 @@ export class Chest extends Phaser.Sprite {
 			return true;
 		});
 
+		//Closing State
+		//Same as the Opening state, but with the opposite animation
 		this.fsm.on(ChestStates.Closing,(from:ChestStates) =>{
 			this.play("close").onComplete.addOnce(()=>{
 				this.fsm.go(ChestStates.Closed);
@@ -130,9 +168,13 @@ export class Chest extends Phaser.Sprite {
 
 		game.add.existing(this);
 
+		//Though the constructor takes in a state at which to start the FSM, the ".on"
+		//handler won't trigger upon initialization. All states are allowed to
+		//transition to themselves.
 		this.fsm.go(ChestStates.Locked);
 	}
 
+	//Code that is necessary when using trimmed, pivoted sprites.
 	private fixPivot(){
 		if(this.animations.currentAnim) {
 			this.pivot.x = this.animations.currentAnim.currentFrame.spriteSourceSizeW / 2;
